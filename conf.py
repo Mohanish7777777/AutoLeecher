8d45a5f4a3ba5ade2616dbfec8f44d9efda382e0
Mohanish <111619160+Mohanish7777777@users.noreply.github.com>
2023-03-19 07:24:13 +0530

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,7 @@
+from apdbot.sample_con import Config
+
+class Config(Config):
+  BOT_TOKEN = "6121682152:AAFFLZ39Hss82yetBB5p34Vl__cdKmGiJyA"
+  APP_ID = 20244111
+  API_HASH = "b76d27da2a4220fe109fe9ef0e866530"
+  AUTH = [-100934368414, -1005572938538]
